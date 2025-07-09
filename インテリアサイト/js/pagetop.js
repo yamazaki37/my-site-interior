d41685bad256420631065b3b6e7113549ad85e84
yamazaki37 <uni.yamazaki37@gmail.com>
2025-07-09 14:34:42 +0900

    Add files via upload

---
+++ インテリアサイト/js/pagetop.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function() {
+    var pagetop = $('.pagetop');
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            pagetop.fadeIn();
+        } else {
+            pagetop.fadeOut();
+        }
+    });
+/*
+    pagetop.click(function () {
+        $('body, html').animate({ scrollTop: 0 }, 500);
+        return false;
+    });
+*/
+	$('a[href^=#]').click(function(){
+		var speed = 900;
+		var href= $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var position = target.offset().top;
+		$("html, body").animate({scrollTop:position}, speed, "swing");
+		return false;
+	});
+
+});
